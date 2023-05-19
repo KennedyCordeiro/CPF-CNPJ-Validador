811b9266d502158d81d0c0f86e7ca323f98741ca
Kennedy Rocha <kennedy.rocha@wings4business.com.br>
2023-05-19 11:17:34 -0300

    Adição e melhoria de componentes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import CpfCnpjField from "./Components/CpfCnpjField";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <i> CPF VALIDATOR</i>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+     
+          <div className="input-group">
+            <label for="inputCheck" className="labelCheck"></label>
+            <CpfCnpjField/>
+          </div>
         <a
           className="App-link"
           href="https://reactjs.org"
