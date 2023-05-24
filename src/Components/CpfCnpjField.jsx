b733e75cf142b57ab0f7f9704fe08b643391c7a3
Kennedy Rocha <kennedy.rocha@wings4business.com.br>
2023-05-24 16:12:17 -0300

    mudanças de layout

--- src/Components/CpfCnpjField.jsx
+++ src/Components/CpfCnpjField.jsx
@@ -4,9 +4,13 @@ import validateTaxNumber from "./ValidateTaxNumber";
 import "../Css/CpfCnpjField.css";
 
 const CpfCnpjField = () => {
+
+  //Constants 
   const [value, setValue] = useState("");
   const [error, setError] = useState(false);
   const [message, setMessage] = useState(false);
+
+
   const formatCpfCnpj = (value) => {
     if (value) {
       value = value.replace(/\D/g, "");
@@ -52,7 +56,7 @@ const CpfCnpjField = () => {
       className="boxContainer"
       style={{ display: "flex", flexDirection: "column" }}
     >
-      <div style={{ marginTop: "10px", marginBottom: "30px" }}>
+      <div style={{ marginTop: "10px" }}>
         <i> CPF VALIDATOR</i>
       </div>
       <div
@@ -64,7 +68,7 @@ const CpfCnpjField = () => {
         }}
       >
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <div style={{ alignSelf: "flex-start", marginBottom: message ? '2px' : '0'}}>
+          <div style={{ alignSelf: "flex-start", marginBottom: '2px' }}>
             <TextField
               label="Insira um CPF ou CNPJ"
               variant="outlined"
@@ -88,29 +92,6 @@ const CpfCnpjField = () => {
             </div>
           )}
         </div>
-        <button
-          onClick={handleClick}
-          disabled={error}
-          style={{ backgroundColor: error ? "red" : "royalblue" }}
-        >
-          <div className="svg-wrapper-1">
-            <div className="svg-wrapper">
-              <svg
-                height="24"
-                width="24"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M0 0h24v24H0z" fill="none"></path>
-                <path
-                  d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </div>
-          </div>
-          <span>Validar</span>
-        </button>
       </div>
     </div>
   );
